5905593d25b8f93f20a711622693837f24a4be48
Ashish Singh <ashish1qwerty1@gmail.com>
2023-05-30 12:16:07 +0530

    Update app

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ options = webdriver.EdgeOptions()
 options.add_argument('--incognito')
 options.add_argument('--headless')
 
-driver = webdriver.Edge('msedgedriver.exe', options=options)
+driver = webdriver.Edge('./msedgedriver.exe', options=options)
 
 def make_transcript_file():
     driver.get("https://www.ted.com/talks/shannon_odell_what_s_the_smartest_age")
